3ba1039dc236a6e971841c4e291f703b573986cc
Viktoria <kvvendetta@gmail.com>
2015-08-21 21:26:30 +0300

    make land main-content-slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-    $('.bxslider').bxSlider({
-        slideWidth: 300,
-        minSlides: 1,
-        maxSlides: 4,
-        slideMargin: 10
-    });
+    $('#bxslider').bxSlider({});
 
-
-    /* input find */
-
-    var inputBlock = $('.header-find'),
-        button = inputBlock.find('.submit'),
-        input = inputBlock.find('.input');
-
-    button.on('click', function() {
-        input.toggleClass('hide');
-    })
 });
